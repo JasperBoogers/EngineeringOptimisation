b9391f5b1bd0743014e21c18938300ea29809b9c
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2022-05-24 14:20:59 +0200

    Create obj_func.m

---
+++ Report/obj_func.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = obj_func(inputArg1,inputArg2)
+%OBJ_FUNC Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
