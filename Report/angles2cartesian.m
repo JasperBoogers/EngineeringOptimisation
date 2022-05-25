d99a8a9ceff93d35b18e57f1a06da2c03f059bf6
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2022-05-25 17:20:32 +0200

    plaatjes gemaakt van alle afmetingen

---
+++ Report/angles2cartesian.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = angles2cartesian(inputArg1,inputArg2)
+%ANGLES2CARTESIAN Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ Report/cartesian2angles.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = cartesian2angles(inputArg1,inputArg2)
+%CARTESIAN2ANGLES Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
