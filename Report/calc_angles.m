fc19bf78adc3123c1cd49269c4d7c2e42ddfed3b
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2022-05-25 17:54:50 +0200

    poging tot calc_angles maken

---
+++ Report/calc_angles.m
@@ -0,0 +1,13 @@
+function [a, b] = calc_angles(s, t, y)
+
+% load parameters
+load_param;
+
+f = @(x) [s*sin(x(1)) - t*sin(x(2)) - y; s*cos(x(1)) + t*cos(x(2)) - ST];
+x0 = rand(2,1)*2*pi;
+res = fsolve(f, x0);
+
+a = mod(res(1), 2*pi);
+b = mod(res(2), 2*pi);
+end
+
---
+++ Report/load_param.m
@@ -0,0 +1,5 @@
+WB = 2.0;   % [m], wheel base
+dWB = 0.5;  % [m], wheel base offset of left wheel w.r.t. right wheel
+B = 1.0;    % [m], front wheel track
+r = 5.0;    % [m], turning radius
+ST = 1.0;   % [m], horizontal distance between wheel attachment and rack-and-pinion attachment
