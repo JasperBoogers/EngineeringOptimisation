a19837248100e674ad1a75ce0c014a6c30818e49
Jasper Boogers <48677568+JasperBoogers@users.noreply.github.com>
2022-05-30 14:35:28 +0200

    add report requirements

--- Report/load_param.m
+++ Report/load_param.m
@@ -3,3 +3,4 @@ dWB = 0.5;  % [m], wheel base offset of left wheel w.r.t. right wheel
 B = 1.0;    % [m], front wheel track
 r = 5.0;    % [m], turning radius
 ST = 1.0;   % [m], horizontal distance between wheel attachment and rack-and-pinion attachment
+x_t = 0.05; % [m], rack-and-pinion displacement
