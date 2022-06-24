9da76226be250d3872f865c3626975453bd59107
Stijn-van-Muiden <62439982+Stijn-van-Muiden@users.noreply.github.com>
2022-06-24 17:08:00 +0200

    full car plot

--- Report/validate_results_full_car.m
+++ Report/validate_results_full_car.m
@@ -7,7 +7,7 @@ load_param;
 
 %%
 
-n=4;
+n=5;
 
 % unpack x into coordinates
 p2y_r = x(1); p2y_l = x(2); p3y_r = x(3); p3y_l = x(4);
@@ -19,7 +19,7 @@ p3l = [p3x_l, p3y_l];
 
 % linearly spaced steering inputs, filter out 0 and 1
 st_f = linspace(0,1,n+1);
-% st_f = st_f(2:end);
+st_f = st_f(2:end);
 [s_r, t_r, a0_r, b0_r, p2r] = calc_length(p2r, p3r, d_r_rt);
 [s_l, t_l, a0_l, b0_l, p2l] = calc_length(p2l, p3l, d_l_rt);
 
@@ -33,8 +33,8 @@ st_f = linspace(0,1,n+1);
         [a_l(i+1), b_l(i+1)] = calc_angles(s_l,t_l, a_l(i), b_l(i), p2l, p3l, -st_f(i));
     end
 
-delta_r = abs(a_r(2:end) - a_r(1));
-delta_l = abs(a_l(2:end) - a_l(1));
+delta_r = a_r(2:end) - a_r(1);
+delta_l = -(a_l(2:end) - a_l(1));
 
 % calcualte differential steer angle = delta_i - delta_o
 r_r = WB./tan(delta_r) + TW/2;
@@ -47,36 +47,33 @@ r = (r_r+r_l)/2;
 d_r_A = atan(WB./(r+TW/2));
 d_l_A = atan((WB-dWB)./(r-TW/2));
 
-figure(2);clf;hold on;
-plot(rad2deg(delta_l), rad2deg(delta_l - delta_r))
-plot(rad2deg(d_l_A), rad2deg(d_l_A - d_r_A))
-title('Ackerman factor')
-xlabel('Steer angle inside wheel \delta_i legt wheel (deg)')
-ylabel('Differential steer angle \Delta\delta = \delta_i - \delta_o (deg)')
-legend('Actual', 'Ideal')
+% variables for nice plotting
+D_wheel = 0.45; % wheel diameter
 
-c = obj_func(delta_r, delta_l, 'right');
-c_af = obj_func_af(delta_r, delta_l);
-
-figure(1); clf;
-subplot(1,2,2); hold on;
-title('Right wheel');
-plot(-[0 p2r(1) p3r(1)], [0 p2r(2) p3r(2)])
+figure(1); clf; 
 for i = 1:n
-    plot(-[0 s_r*cos(a_r(i)) s_r*cos(a_r(i))+t_r*cos(b_r(i))], [0   s_r*sin(a_r(i)) s_r*sin(a_r(i))+t_r*sin(b_r(i))])
+    delta_r_plot = delta_r(i) + pi/2;
+    delta_l_plot = delta_l(i) + pi/2;
+
+    % rear wheel
+    plot(0, 0, 'b*'); hold on;
+    line([0 0], D_wheel*[1 -1], 'color', 'k')
+    line(10*[-1 1], [0 0])
+    
+    % front right
+    plot(0, WB, 'b*') % wheel point
+    line(D_wheel*cos(delta_r_plot)*[-1 1], D_wheel*sin(delta_r_plot)*[-1 1]+WB, 'color', 'k') % wheel
+    line(10*cos(delta_r_plot+pi/2)*[-1 1], 10*sin(delta_r_plot+pi/2)*[-1 1]+WB, 'color', 'r') % extending line
+    
+    
+    % front left
+    plot(-TW, WB-dWB, 'b*') % wheel point
+    line(D_wheel*cos(delta_l_plot)*[-1 1]-TW, D_wheel*sin(delta_l_plot)*[-1 1]+WB-dWB, 'color', 'k') % wheel
+    line(10*cos(delta_l_plot+pi/2)*[-1 1]-TW, 10*sin(delta_l_plot+pi/2)*[-1 1]+WB-dWB, 'color', 'r') % extending line
+    hold off; axis('equal');
+    drawnow;
+    pause;
 end
-axis('equal')
-legend()
 
 
-subplot(1,2,1); hold on;
-title('Left wheel')
 
-plot([0 p2l(1) p3l(1)], [0 p2l(2) p3l(2)])
-for i=1:n
-    plot([0 s_l*cos(a_l(i)) s_l*cos(a_l(i))+t_l*cos(b_l(i))], [0   s_l*sin(a_l(i)) s_l*sin(a_l(i))+t_l*sin(b_l(i))])
-end
-axis('equal')
-legend()
-
-% function plot_ackerman_factor()
